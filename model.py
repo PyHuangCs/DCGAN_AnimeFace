2026e845a7f33f75dc70815e1a4f1c8953e3e7d8
Peiyuan Huang <108218584+PyHuangCs@users.noreply.github.com>
2026-05-26 10:52:04 +0800

    Refactor DCGAN model definitions

--- model.py
+++ model.py
@@ -1,68 +1,88 @@
-import torch
+"""DCGAN model definitions for anime face generation."""
+
 import torch.nn as nn
-from dataprocess import to_device,device
 
-# 判别器网络
-# Discriminator Network
-discriminator = nn.Sequential(
-    # in: 3 x 64 x 64
-    
-    nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=False),
-    nn.BatchNorm2d(64),
-    nn.LeakyReLU(0.2, inplace=True),
-    # out: 64 x 32 x 32
 
-    nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
-    nn.BatchNorm2d(128),
-    nn.LeakyReLU(0.2, inplace=True),
-    # out: 128 x 16 x 16
+LATENT_SIZE = 128
 
-    nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
-    nn.BatchNorm2d(256),
-    nn.LeakyReLU(0.2, inplace=True),
-    # out: 256 x 8 x 8
 
-    nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
-    nn.BatchNorm2d(512),
-    nn.LeakyReLU(0.2, inplace=True),
-    # out: 512 x 4 x 4
+class Discriminator(nn.Module):
+    """DCGAN discriminator for 64x64 RGB images."""
 
-    nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False),
-    # out: 1 x 1 x 1
+    def __init__(self, image_channels: int = 3, feature_maps: int = 64):
+        super().__init__()
+        self.net = nn.Sequential(
+            # input: image_channels x 64 x 64
+            nn.Conv2d(image_channels, feature_maps, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(feature_maps),
+            nn.LeakyReLU(0.2, inplace=True),
+            # feature_maps x 32 x 32
 
-    nn.Flatten(),
-    nn.Sigmoid()
-)
-discriminator = to_device(discriminator, device)
-latent_size = 128
+            nn.Conv2d(feature_maps, feature_maps * 2, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(feature_maps * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # feature_maps*2 x 16 x 16
 
-# 生成器网络
-#Generator Network
-generator = nn.Sequential(
-    # in: latent_size x 1 x 1
+            nn.Conv2d(feature_maps * 2, feature_maps * 4, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(feature_maps * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # feature_maps*4 x 8 x 8
 
-    nn.ConvTranspose2d(latent_size, 512, kernel_size=4, stride=1, padding=0, bias=False),
-    nn.BatchNorm2d(512),
-    nn.ReLU(True),
-    # out: 512 x 4 x 4
+            nn.Conv2d(feature_maps * 4, feature_maps * 8, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(feature_maps * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # feature_maps*8 x 4 x 4
 
-    nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=False),
-    nn.BatchNorm2d(256),
-    nn.ReLU(True),
-    # out: 256 x 8 x 8
+            nn.Conv2d(feature_maps * 8, 1, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.Flatten(),
+            nn.Sigmoid(),
+        )
 
-    nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1, bias=False),
-    nn.BatchNorm2d(128),
-    nn.ReLU(True),
-    # out: 128 x 16 x 16
+    def forward(self, x):
+        return self.net(x)
 
-    nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False),
-    nn.BatchNorm2d(64),
-    nn.ReLU(True),
-    # out: 64 x 32 x 32
 
-    nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1, bias=False),
-    nn.Tanh()
-    # out: 3 x 64 x 64
-)
-generator = to_device(generator, device)
+class Generator(nn.Module):
+    """DCGAN generator for 64x64 RGB images."""
+
+    def __init__(self, latent_size: int = LATENT_SIZE, image_channels: int = 3, feature_maps: int = 64):
+        super().__init__()
+        self.net = nn.Sequential(
+            # input: latent_size x 1 x 1
+            nn.ConvTranspose2d(latent_size, feature_maps * 8, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(feature_maps * 8),
+            nn.ReLU(True),
+            # feature_maps*8 x 4 x 4
+
+            nn.ConvTranspose2d(feature_maps * 8, feature_maps * 4, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(feature_maps * 4),
+            nn.ReLU(True),
+            # feature_maps*4 x 8 x 8
+
+            nn.ConvTranspose2d(feature_maps * 4, feature_maps * 2, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(feature_maps * 2),
+            nn.ReLU(True),
+            # feature_maps*2 x 16 x 16
+
+            nn.ConvTranspose2d(feature_maps * 2, feature_maps, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(feature_maps),
+            nn.ReLU(True),
+            # feature_maps x 32 x 32
+
+            nn.ConvTranspose2d(feature_maps, image_channels, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.Tanh(),
+            # image_channels x 64 x 64
+        )
+
+    def forward(self, z):
+        return self.net(z)
+
+
+def weights_init(module):
+    """Initialize DCGAN weights following the original DCGAN convention."""
+    classname = module.__class__.__name__
+    if classname.find("Conv") != -1:
+        nn.init.normal_(module.weight.data, 0.0, 0.02)
+    elif classname.find("BatchNorm") != -1:
+        nn.init.normal_(module.weight.data, 1.0, 0.02)
+        nn.init.constant_(module.bias.data, 0)
